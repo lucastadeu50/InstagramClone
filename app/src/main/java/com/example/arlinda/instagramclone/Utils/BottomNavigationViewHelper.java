5648273ba1ab9f03d4df7f12b9fe15b3649db181
lucas <lucastadeu50@hotmail.com>
2019-01-09 17:24:34 -0200

    Parte 4

--- app/src/main/java/com/example/arlinda/instagramclone/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/com/example/arlinda/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -1,10 +1,22 @@
 package com.example.arlinda.instagramclone.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.example.arlinda.instagramclone.HomeActivity;
+import com.example.arlinda.instagramclone.LikesActivity;
+import com.example.arlinda.instagramclone.ProfileActivity;
+import com.example.arlinda.instagramclone.R;
+import com.example.arlinda.instagramclone.SearchActivity;
+import com.example.arlinda.instagramclone.ShareActivity;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class BottomNavigationViewHelper {
+
     private static final String TAG = "BottomNavigationViewHel";
 
     public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
@@ -13,5 +25,43 @@ public class BottomNavigationViewHelper {
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.ic_house:
+                        Intent intent1 = new Intent(context, HomeActivity.class);//ACTIVITY_NUM = 0
+                        context.startActivity(intent1);
+                        break;
+
+                    case R.id.ic_search:
+                        Intent intent2  = new Intent(context, SearchActivity.class);//ACTIVITY_NUM = 1
+                        context.startActivity(intent2);
+                        break;
+
+                    case R.id.ic_circle:
+                        Intent intent3 = new Intent(context, ShareActivity.class);//ACTIVITY_NUM = 2
+                        context.startActivity(intent3);
+                        break;
+
+                    case R.id.ic_alert:
+                        Intent intent4 = new Intent(context, LikesActivity.class);//ACTIVITY_NUM = 3
+                        context.startActivity(intent4);
+                        break;
+
+                    case R.id.ic_android:
+                        Intent intent5 = new Intent(context, ProfileActivity.class);//ACTIVITY_NUM = 4
+                        context.startActivity(intent5);
+                        break;
+                }
+
+
+                return false;
+            }
+        });
     }
 }
